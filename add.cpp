7b7ceac6fdf885cdb1b0dca6eba51c7bd2007b72
Sarai75 <moaj080602mmcrlsa3@cbtis217.edu.mx>
2024-10-22 20:43:42 -0600

    Agregando a los colaboradores

--- add.cpp
+++ add.cpp
@@ -1,8 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-    //lectura
-	int a, b, c; cin >> a >> b;
-	//impresion
-	cout << a + b << "\n";
-}
+/*
+LOPEZ MORENO KEVIN GAEL
+LOPEZ YAÑEZ CINTIA GUADALUPE
+MARTINEZ GUZMÁN HUGO ANTONIO
+MASCOTE REYES EMMANUEL
+MORALES ALARCÓN JESSICA SARAI
+*/
+int main() 
+	{
+ 	   //lectura
+	   int a, b, c; cin >> a >> b;
+	   //impresion
+	   cout << a + b << "\n";
+	}
